44e81b2a2685a9e1c6fe6dfaac62143b69a82b3b
mithra <mithrargowda@gmail.com>
2023-10-26 16:23:46 +0530

    first commit

---
+++ 26-10-2023/comments.php
@@ -0,0 +1,12 @@
+<?php
+$a="hello world";
+$b= "mithra";
+echo("$a<br>");
+echo("$a<br>");
+print"mithra<br>".$b."<br>";
+print("hello world"."<br>".$a);
+echo("hello world","<br>",$a);
+//echo"<br>";
+echo $a;
+//echo"<br>";
+?>
